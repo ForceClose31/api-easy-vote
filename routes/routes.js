1697ea55439852ac4693d47984fb04ae852fc48d
ForceClose31 <forceclose31@gmail.com>
2024-12-08 03:15:32 +0700

    feat: GET event all

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const { login } = require('../controllers/userController');
+const { getEventByCode, getEventAll } = require('../controllers/eventController');
 const { getCandidates } = require('../controllers/candidateController');
 const authMiddleware = require('../middleware/authMiddleware');
 const router = express.Router();
 
+router.post('/login', login);
+
+router.get('/event/:code', authMiddleware, getEventByCode);
+router.get('/event', authMiddleware, getEventAll);
+
 router.get('/candidate', authMiddleware, getCandidates);
+
 
 module.exports = router;
