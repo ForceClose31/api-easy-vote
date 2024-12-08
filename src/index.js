5e2c1ccdded9836130c7c069543df6c2816f6edc
pentahead <dhiyaafaruq@gmail.com>
2024-12-09 05:01:23 +0700

    add: CORS

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const routes = require('../routes/routes');
+const express = require("express");
+const dotenv = require("dotenv");
+const routes = require("../routes/routes");
+const cors = require("cors");
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
-
+app.use(cors());
 app.use(express.json());
 
-app.use('/api', routes);
+app.use("/api", routes);
 
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
